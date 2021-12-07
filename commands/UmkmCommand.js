508bca3765e8eec8f51411f162c673cda7f6e5ba
muhfirdaus19 <firdaus.ex19@gmail.com>
2021-12-07 09:39:10 +0800

    inline query with inline keyboard

--- commands/UmkmCommand.js
+++ commands/UmkmCommand.js
@@ -2,6 +2,8 @@ const db = require("../config/Databases");
 const umkm = db.umkm;
 const fetch = require('node-fetch').default
 const axios = require('axios');
+const { Markup } = require('telegraf')
+
 
 function resto(bot) {
     bot.inlineQuery(/umkm\s.+/, async (ctx) => {
@@ -16,17 +18,26 @@ function resto(bot) {
                 title: item.name,
                 description: `${item.description} \n ${item.address} \n ${item.telp}`,
                 input_message_content: {
-                    message_text: `${item.name} \n ${item.description}`
+                    message_text: `${item.name} \n ${item.description}`,
+                    parse_mode: 'Markdown'
                 },
                 hide_url: false,
-                thumb_url: item.image
+                thumb_url: item.image,
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            { text: 'More info', callback_data: 'moreinfo' },
+                            { text: 'Add to favorite', callback_data: 'addtofavorite' }
+                        ],
+                ]},
             }
         })
         return await ctx.answerInlineQuery(result)
     });
-    bot.on('chosen_inline_result', ({ chosenInlineResult }) => {
-        console.log('chosen inline result', chosenInlineResult)
-    })
+
+    // bot.on('chosen_inline_result', (ctx) => {
+    //     console.log(ctx)
+    // })
 }
 
 module.exports = resto;
